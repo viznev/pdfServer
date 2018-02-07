3d65c22a1b6c1aa6b193590ee1b39a20ea9fc6ed
viznev <13683232+viznev@users.noreply.github.com>
2018-02-06 21:25:46 -0600

    - increased received POST data limit

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const fs = require('fs')
 
 const PDFDocument = require('pdfkit')
 
-app.use(express.json({limit: '300kb'}));
-app.use(express.urlencoded({limit: '300kb', extended: true}));
+app.use(express.json({limit: '1mb'}));
+app.use(express.urlencoded({limit: '1mb', extended: true}));
 
 app.get('/', (req, res, next) => {
     res.send('Hello World!');
