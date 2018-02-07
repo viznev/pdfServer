1900669a72c38867f9a10c421e8d121a122fb97c
viznev <13683232+viznev@users.noreply.github.com>
2018-02-06 21:02:42 -0600

    cleaned up code & added comments

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const fs = require('fs')
 const PDFDocument = require('pdfkit')
 
 app.use(express.json({limit: '300kb'}));
-app.use(express.urlencoded({limit: '300kb'}));
+app.use(express.urlencoded({limit: '300kb', extended: true}));
 
 app.use(express.json());
 app.use(express.urlencoded());
 
 app.use(function (req, res, next) {
+    // fix this to only allow localhost??
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next()
@@ -29,11 +30,6 @@ app.post('/', (req, res, next) => {
         page2 = req.body.page2,
         playerdata = req.body.playerdata;
 
-    // console.log('body output: ')
-    // console.log(req.body)
-    // console.log('files output: ')
-    // console.log(req.files)
-
     console.log('bytes: '+req.socket.bytesRead)
 
     if ((page1) && (page2) && (playerdata)) {
@@ -43,19 +39,26 @@ app.post('/', (req, res, next) => {
         // pipe the output to a file
         pdf.pipe(fs.createWriteStream('output.pdf'))
 
-        //add an image to the first page
+        // add an image to the first page
         pdf.image(page1, 0, 0, {fit:[pdf.page.width, pdf.page.height]})
 
+        // create a second page
         pdf.addPage()
 
+         // add an image to the second page
         pdf.image(page2, 0, 0, {fit:[pdf.page.width, pdf.page.height]})
 
+        // create a third page
         pdf.addPage()
 
+        // add the playerdata to the third page - the replace functions just prettify it
         pdf.text(JSON.stringify(playerdata).replace(/,/g, ",\n    ").replace("{","{\n    ").replace('}','\n}'))
 
         // finalize PDF file
         pdf.end()
+
+        // let the sender know we got everything we need
+        // (should this be moved to the beginning in case a failure may block it and cause the sender to timeout while waiting for a response?)
         res.send('{"result":"success"}')
     } else {
         res.send('{"result":"error","description":"did not receive appropriate data"}')
